71355161051abab608f35e99359f7a8e4adee6f0
EyjafjallaLove <2198197448@qq.com>
2026-05-12 18:43:25 +0800

    feat(core): Implement main frontend core components and page structure

---
+++ art-text-generator/electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  })
+
+  // 开发时加载 Vite 开发服务器，打包后加载本地文件
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    win.webContents.openDevTools() // 可注释掉不让用户看到控制台
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
